15697a86295ebb7a76e19bcd6cc12622afea5daf
Martynas Girdvainis <martynas.girdvainis@nfq.lt>
2017-11-14 21:35:16 +0200

    Add workshop demo changes

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import Button from '../../components/Button';
+import Card from '../../components/Card';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.onAddCard = this.onAddCard.bind(this);
+    this.onRemoveAllCards = this.onRemoveAllCards.bind(this);
+
+    this.state = {
+      cards: [
+        { value: 1 }
+      ]
+    };
+  }
+  onAddCard() {
+    const { cards } = this.state;
+
+    cards.push({ value: 1 });
+
+    this.setState({
+      cards
+    });
+  }
+  onRemoveAllCards() {
+    this.setState({
+      cards: []
+    })
+  }
   render() {
     return (
       <div className="app-container">
@@ -8,16 +36,21 @@ export default class App extends React.Component {
 
           <div className="play-area-cards">
             <div className="play-area-cards__deck">
-              Deck
+              <Card deck value={52}/>
             </div>
 
             <div className="play-area-cards__items">
-              Cards
+              {this.state.cards.map((card, i) => {
+                return (
+                  <Card key={i} value={card.value}/>
+                );
+              })}
             </div>
           </div>
 
           <div className="play-area__actions">
-            Buttons
+            <Button title="Add card" click={this.onAddCard}/>
+            <Button title="Remove cards" click={this.onRemoveAllCards}/>
           </div>
 
         </div>
