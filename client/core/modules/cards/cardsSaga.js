9afa07626caf4e0a8816e156512fb5b382eccd72
Martynas Girdvainis <martynas.girdvainis@nfq.lt>
2018-07-10 19:01:34 +0300

    Add challange description

--- client/core/modules/cards/cardsSaga.js
+++ client/core/modules/cards/cardsSaga.js
@@ -4,6 +4,25 @@ import { constants } from '../../../core/constants';
 function* onRequestCard() {
   const state = yield select();
 
+  // uzduotis 1:
+  // paspaudus mygtukas iskviesti requesta is https://deckofcardsapi.com/ ir gauti viena korta
+  // ta korta ideti i store (papildyti turima lista)
+  // kortas isvesti pagr. lange
+
+  // uzduotis 2:
+  // kol vyksta korteles requestas gavimo, rodyti "loading..." jos vietoje
+
+  // uzduotis 3: automatizuoti zaidima
+  // sukurti mygtuka, kuri paspaudus viena karta, kviestu korteles tol, kol bus surinkta 21
+  //    bartukai, damos, karaliai verti 10
+  //    tuzai 1
+  // tarp kiekvienos korteles iskvietimo turi buti 500ms pauze
+  // jeigu virsyta 21, tada turi automatiskai restartuoti zaidima
+  // procesas turi vykti, kol bus pasiekta lygiai 21, tada stop
+
+  // bonus 3 uzduociai:
+  // tuzas vertas 11, TACIAU jeigu su 11 virsyja 21, tada jis vertas 1
+
   // put - tas pats, kas dispatch
   // call - requestinti f-jas, generatorius, requestus
 }
